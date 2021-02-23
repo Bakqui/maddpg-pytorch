689d7ef18ee8c7a655892804b68d3660cc1f6912
Bakqui <bq1739@gmail.com>
2021-02-23 19:56:00 +0900

    add_local_envs

--- utils/make_env.py
+++ utils/make_env.py
@@ -31,18 +31,31 @@ def make_env(scenario_name, benchmark=False, discrete_action=False):
     '''
     from multiagent.environment import MultiAgentEnv
     import multiagent.scenarios as scenarios
+    import envs.mpe_scenarios as new_scenarios
 
     # load scenario from script
-    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    try:
+        scenario = scenarios.load(scenario_name + ".py").Scenario()
+    except:
+        scenario = new_scenarios.load(scenario_name + ".py").Scenario()
     # create world
     world = scenario.make_world()
     # create multiagent environment
+    if hasattr(scenario, 'post_step'):
+        post_step = scenario.post_step
+    else:
+        post_step = None
     if benchmark:        
-        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward,
-                            scenario.observation, scenario.benchmark_data,
+        env = MultiAgentEnv(world, reset_callback=scenario.reset_world,
+                            reward_callback=scenario.reward,
+                            observation_callback=scenario.observation,
+                            post_step_callback=post_step,
+                            info_callback=scenario.benchmark_data,
                             discrete_action=discrete_action)
     else:
-        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward,
-                            scenario.observation,
+        env = MultiAgentEnv(world, reset_callback=scenario.reset_world,
+                            reward_callback=scenario.reward,
+                            observation_callback=scenario.observation,
+                            post_step_callback=post_step,
                             discrete_action=discrete_action)
     return env
